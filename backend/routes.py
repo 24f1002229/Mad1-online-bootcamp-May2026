32eae2241aba8c461c1a4e6efbda00d7e882b8df
Palak887 <24f1002229@ds.study.iitm.ac.in>
2026-06-25 12:08:38 +0530

    Session4

--- backend/routes.py
+++ backend/routes.py
@@ -13,9 +13,9 @@ def Login():
         password = request.form.get("password")
         user = User.query.filter_by(email=email, password=password).first()
         if user and user.role == 1:
-            return redirect(url_for("user"))
+            return redirect(url_for("user", email=email))
         elif user and user.role == 0:
-            return redirect(url_for("admin"))
+            return redirect(url_for("admin", email=email))
         else:
             return render_template("Login.html", msg= "Invalid user Register yourself first")
         
@@ -36,10 +36,10 @@ def Register():
         return render_template("Login.html", msg="you can continue with login!!")
     return render_template("Register.html")
         
-@app.route("/admin")
-def admin():
+@app.route("/Admin/<email>")
+def admin(email):
     courses = get_course()
-    return render_template("admin.html", courses=courses)
+    return render_template("admin.html", courses=courses, email=email)
 
 
 def get_course(id=None):
@@ -48,7 +48,43 @@ def get_course(id=None):
     return Course.query.all()
 
 
-@app.route("/user")
-def user():
+@app.route("/user/<email>")
+def user(email):
     courses = get_course()
-    return render_template("User.html", courses=courses)
+    return render_template("User.html", courses=courses, email=email)
+
+@app.route("/add_course/<email>", methods=["GET","POST"])
+def add_course(email):
+    if request.method == "POST":
+        id = request.form.get("id")
+        name = request.form.get("name")
+        description = request.form.get("description")
+        newcourse = Course(id=id, name=name, description=description)
+        db.session.add(newcourse)
+        db.session.commit()
+        return redirect(url_for("admin", email=email))
+    return render_template("add_course.html", email=email)
+
+
+@app.route("/edit_course/<id>/<email>", methods=["GET","POST"])
+def edit_course(email, id):
+    c = get_course(id)
+    if request.method=="POST":
+        id = request.form.get("id")
+        name = request.form.get("name")
+        description = request.form.get("description")
+        c.id = id
+        c.name = name
+        c.description = description
+        db.session.commit()
+        return redirect(url_for("admin", email=email))
+    return render_template("edit_course.html", email=email, course = c)
+
+
+@app.route("/delete_course/<id>/<email>", methods=["GET","POST"])
+def delete_course(email,id):
+    dc = get_course(id)
+    db.session.delete(dc)
+    db.session.commit()
+    return redirect(url_for("admin", email=email))
+        
